615c3b956c14ca4c20d34a9cd842f5deb579e9c6
yoshihisa <yoshihisa.drum@gmail.com>
2020-08-08 10:30:47 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by USER on 2020/08/08.
+//  Copyright © 2020 Yoshihisa Iijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
